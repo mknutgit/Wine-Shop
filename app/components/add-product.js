37b8fc3d30db447496b17747f6a5e9075d070e00
jordan meier and matt knutson <jbirdmeier@gmail.com>
2016-04-06 14:14:42 -0700

    add a select menu when creating a new product for varietal

--- app/components/add-product.js
+++ app/components/add-product.js
@@ -2,16 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   addNewProduct: false,
+  chosenVarietal: null,
   actions: {
     productFormShow () {
       this.set('addNewProduct', true);
+    },
+    chooseVarietal () {
+      this.set('chosenVarietal', event.target.value);
     },
     addProduct() {
       var params = {
         producer: this.get('producer') ? this.get('producer'): "",
         image: this.get('image') ? this.get('image'): "",
         description: this.get('description') ? this.get('description'): "",
-        varietal: this.get('varietal') ? this.get('varietal'): "",
+        varietal: this.get('chosenVarietal'),
         cost: this.get('cost') ? this.get('cost'): ""
     };
     this.set('addNewProduct', false);
