0f7c49baf45243250bcc5851d7dff66e694ccb68
jordan meier and matt knutson <mknut001@gmail.com>
2016-04-06 09:39:40 -0700

    add a computed property for totalcost in display-cart component

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  totalCost: Ember.computed('shoppingCart.items', function() {
+    var total= 0;
+    for (var i= 0; i < this.get('shoppingCart.items.length'); i++) {
+      // debugger;
+      total += this.get('shoppingCart.items')[i].get('cost');
+    }
+    return total;
+  })
 });
