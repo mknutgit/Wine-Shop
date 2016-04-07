2c9745699e15329244af279c847b814ba49dcc63
matt knutson <mknut001@gmail.com>
2016-04-07 09:20:20 -0700

    allow removal of product and deduct from total cost

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
-  totalCost: Ember.computed('shoppingCart.items', function() {
+  totalCost: Ember.computed('shoppingCart.items.[]', function() {
     var total= 0;
     for (var i= 0; i < this.get('shoppingCart.items.length'); i++) {
       total += this.get('shoppingCart.items')[i].get('cost');
